f285d05c2bc925a25f3ff3b077f699c706b99265
Gabriel Jose <luckygb21@gmail.com>
2021-09-06 21:40:23 -0300

    - Mudança nas validações; - Diminui as msg na tela; - Pontuação agora aparece; - Melhor de 3 rodadas;

--- Python/Rock_Paper_Scissors/main.py
+++ Python/Rock_Paper_Scissors/main.py
@@ -7,16 +7,26 @@ class MiniGame:
     options = ["Stone", "Paper", "Scissors"]
     punctuation_user = 0
     punctuation_bot = 0
+    sum_punctuation = 0
+
+    def resetting_score(self):
+        self.punctuation_user = 0
+        self.punctuation_bot = 0
+        self.sum_punctuation = 0
 
     @classmethod
     def validate_info_user(cls, userChoice):
         try:
+            # num_str = str(userChoice)
             if userChoice == "" or userChoice is None:
-                print("[-] Please type something so we can play! :) ")
+                print("\n[-] Please type something so we can play! :) \n")
+                return True
             if not userChoice.isnumeric():
-                print("[-] Please type only number!!! :/ ")
-            # if userChoice != 0 or userChoice != 1 or userChoice != 2:
-                #print("[-] Please choose only of the options above! :/ ")
+                print("\n[-] Please type only number!!! :/ \n")
+                return True
+            if int(userChoice) > 2:
+                print("\n[-] Please choose from 0 to 2!!!!\n")
+                return True
         except (IndexError, Exception, BaseException, ValueError):
             print("[-] An unexpected error has occurred! :/ ")
 
@@ -37,15 +47,15 @@ class MiniGame:
         # User
         if self.options[choose_user] == "Stone" and self.options[choose_bot] == "Scissors":
             print(f"[+] Congratulations {self.user_name} you won!!!! +1 Score!!!")
-            MiniGame.punctuation_user += 1
+            self.punctuation_user += 1
             return
         if self.options[choose_user] == "Paper" and self.options[choose_bot] == "Stone":
             print(f"[+] Congratulations {self.user_name} you won!!!! +1 Score!!!")
-            MiniGame.punctuation_user += 1
+            self.punctuation_user += 1
             return
         if self.options[choose_user] == "Scissors" and self.options[choose_bot] == "Paper":
             print(f"[+] Congratulations {self.user_name} you won!!!! +1 Score!!!")
-            MiniGame.punctuation_user += 1
+            self.punctuation_user += 1
             return
 
         # Bot
@@ -64,42 +74,59 @@ class MiniGame:
 
     def main(self):
         try:
-            print(self.punctuation_user)
             you_continue = True
-            print(f"[+] Hi! How are you?")
-            print(f"[+] Let's start our game {self.user_name}?")
+            print(f"[+] Hi! How are you? Let's start our game {self.user_name}?\n")
             while you_continue:
                 print(f"[+] Chooses one of the options below: ")
                 print(f"    => [0] for Stone(Pedra)")
                 print(f"    => [1] for Paper(Papel)")
                 print(f"    => [2] for Scissors(Tesoura)")
+
+                if self.punctuation_user > 0 or self.punctuation_bot > 0:
+                    print(f"\n[+] Your punctuation: {self.punctuation_user}")
+                    print(f"[+] BOT punctuation: {self.punctuation_bot}")
+
                 userChoice = input("[+] What will be your choice? ")
-                print("[+] Thinking....")
-                MiniGame.validate_info_user(userChoice)
-                choose_bot = MiniGame().robot_choice()
-                MiniGame().result_of_choices(choose_bot, int(userChoice))
-                result_continue = input("[+] Do you wish to continue? [Y(yes) / N(No)]")
-                if result_continue.upper() == "Y":
-                    print("[+] Alright let's continue!!! ")
+                if self.validate_info_user(userChoice):
+                    continue
+                print("[+] Thinking....\n")
+                choose_bot = self.robot_choice()
+                self.result_of_choices(choose_bot, int(userChoice))
+                self.sum_punctuation = self.punctuation_user + self.punctuation_bot
+
+                if self.sum_punctuation == 3 or self.punctuation_user == 3 or self.punctuation_bot == 3:
                     if self.punctuation_user > self.punctuation_bot:
-                        print("     [+] You are winning me!!! But i will turn this game around :) ")
-                    elif self.punctuation_user == self.punctuation_bot:
-                        print("     [+] We are tied!!")
+                        print(f"[+] Your punctuation: {self.punctuation_user}")
+                        print(f"[-] BOT punctuation: {self.punctuation_bot}")
+                        print("     [+] You won me !!! next time i will be the winner :c")
                     else:
-                        print(f"    [+] Hahaha I'm winning, if you wanted to win me you'll have to make an affort! Loser {self.user_name} ")
-                    you_continue = True
-                elif result_continue.upper() == "N":
-                    print("     [+] It was good to play with you, the score was like this: ")
-                    print(f"        {self.user_name}: {self.punctuation_user}")
-                    print(f"        bot_: {self.punctuation_bot}")
-                    you_continue = False
-                else:
-                    print("[-] Please choose between Y or N.")
+                        print(f"[+] Your punctuation: {self.punctuation_user}")
+                        print(f"[+] BOT punctuation: {self.punctuation_bot}")
+                        print(f"     [-] Hahaha I won, if you wanted to beat me you'll have to make an effort! Loser "
+                              f"{self.user_name}")
 
+                    result_continue = input("[+] Do you wish to continue? [Y(yes) / N(No)]")
+                    if result_continue.upper() == "Y":
+                        self.resetting_score()
+                        you_continue = True
+                    elif result_continue.upper() == "N":
+                        print("\n[+] It was good to play with you, see you later!")
+                        self.resetting_score()
+                        you_continue = False
+                    else:
+                        print("[-] Please choose between Y or N.")
+                        return
 
-        except (IndexError, Exception, BaseException, ValueError) as err:
-            print("[-] An unexpected error has occurred! :/ " + str(err))
+        except (IndexError, Exception, BaseException, ValueError):
+            print("[-] An unexpected error has occurred! :/ ")
 
 
 if __name__ == "__main__":
     MiniGame().main()
+
+"""
+-> Verifica se o numero é repetido
+# if len(str(userChoice)) >= 1 and (len(str(userChoice)) * num_str[0]) == userChoice:
+#     print("\n[-] Please choose from 0 to 2!!!!\n")
+#     return True
+"""
